4a4cbc365584281360ca4448a43604513eb8d600
Jeremzzz <34739815+Jeremzzz@users.noreply.github.com>
2018-04-15 18:05:59 +0200

    Add files via upload

---
+++ code/accelero.ino
@@ -0,0 +1,12 @@
+float t=0;
+void setup(){ 
+Serial.begin(9600);
+} 
+void loop() {
+Serial.print("$");
+Serial.print((int)(analogRead(A0)/0.2048));  
+Serial.println(";"); 
+t=(int)(analogRead(A0)/0.2048);
+Serial.println(asin((t-1600)/330));
+delay(100); 
+}
---
+++ code/hcr.ino
@@ -0,0 +1,18 @@
+const int trig=9;
+const int echo=8;
+int lecture_echo=0;
+void setup() {
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  digitalWrite(trig,HIGH); //génération de l'impulsion Trig
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  lecture_echo=pulseIn(echo,HIGH);
+  Serial.println(lecture_echo*0.017);
+
+}
